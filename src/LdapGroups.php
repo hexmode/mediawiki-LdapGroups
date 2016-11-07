41e9083c7151c2db2c9dc76b0b912e8e288342e9
Mark A. Hershberger <mah@nichework.com>
2016-11-07 15:12:59 -0500

    Remove references to Active Directory

--- src/LdapGroups.php
+++ src/LdapGroups.php
@@ -7,26 +7,26 @@ use MWException;
 use User;
 
 class LdapGroups {
-	protected $ad;
+	protected $ldap;
 	protected $param;
-	protected $adGroupMap;
+	protected $ldapGroupMap;
 	protected $mwGroupMap;
 
 	public function __construct( $param ) {
 		wfDebug( __METHOD__ );
-		$this->ad = ldap_connect( $param['server'] );
-		if ( !$this->ad ) {
+		$this->ldap = ldap_connect( $param['server'] );
+		if ( !$this->ldap ) {
 			throw new MWException( "Error Connecting to LDAP server.!" );
 		}
 		$this->param = $param;
 
-		ldap_set_option( $this->ad, LDAP_OPT_REFERRALS, 0 );
-		ldap_set_option( $this->ad, LDAP_OPT_PROTOCOL_VERSION, 3 );
-		if ( !ldap_bind( $this->ad, $this->param['user'],
+		ldap_set_option( $this->ldap, LDAP_OPT_REFERRALS, 0 );
+		ldap_set_option( $this->ldap, LDAP_OPT_PROTOCOL_VERSION, 3 );
+		if ( !ldap_bind( $this->ldap, $this->param['user'],
 						 $this->param['pass'] )
 		) {
 			throw new MWException( "Couldn't bind to LDAP server: " .
-								   ldap_error( $this->ad ) );
+								   ldap_error( $this->ldap ) );
 		}
 
 		$this->setupGroupMap();
@@ -48,9 +48,9 @@ class LdapGroups {
 				$wgGroupPermissions[$name] = $wgGroupPermissions['user'];
 			}
 			foreach ($DNs as $key) {
-				$lowAD = strtolower( $key );
-				$this->mwGroupMap[ $name ][] = $lowAD;
-				$this->adGroupMap[ $lowAD ] = $name;
+				$lowLDAP = strtolower( $key );
+				$this->mwGroupMap[ $name ][] = $lowLDAP;
+				$this->ldapGroupMap[ $lowLDAP ] = $name;
 			}
 		}
 
@@ -83,34 +83,34 @@ class LdapGroups {
 		return new LdapGroups( $data );
 	}
 
-	protected function doADSearch( $match ) {
+	protected function doLDAPSearch( $match ) {
 		$basedn = $this->param['basedn'];
 
-		wfProfileIn( __METHOD__ . " - AD Search" );
+		wfProfileIn( __METHOD__ . " - LDAP Search" );
 		$runTime = -microtime( true );
-		$res = ldap_search( $this->ad, $basedn, $match, [ "*" ] );
+		$res = ldap_search( $this->ldap, $basedn, $match, [ "*" ] );
 		if ( !$res ) {
 			wfProfileOut( __METHOD__ );
-			throw new MWException( "Error in AD search: " .
-								   ldap_error( $this->ad ) );
+			throw new MWException( "Error in LDAP search: " .
+								   ldap_error( $this->ldap ) );
 		}
 
-		$entry = ldap_get_entries( $this->ad, $res );
+		$entry = ldap_get_entries( $this->ldap, $res );
 		$runTime += microtime( true );
-		wfProfileOut( __METHOD__ . " - AD Search" );
-		wfDebugLog( __CLASS__, "Ran AD search in $runTime seconds.\n" );
+		wfProfileOut( __METHOD__ . " - LDAP Search" );
+		wfDebugLog( __CLASS__, "Ran LDAP search in $runTime seconds.\n" );
 		return $entry;
 	}
 
-	public function fetchADData( User $user ) {
+	public function fetchLDAPData( User $user ) {
 		$email = $user->getEmail();
 
 		if( !$email ) {
 			// Fail early
 			throw new MWException( "No email found for $user" );
 		}
-		wfDebug( __METHOD__ . ": Fetching user data for $user from AD\n" );
-		$entry = $this->doADSearch( $this->param['searchattr'] .
+		wfDebug( __METHOD__ . ": Fetching user data for $user from LDAP\n" );
+		$entry = $this->doLDAPSearch( $this->param['searchattr'] .
 									"=" . $user->getEmail() );
 
 		if ( $entry['count'] === 0 ) {
@@ -124,36 +124,36 @@ class LdapGroups {
 								   "with the ID: $user" );
 		}
 
-		$this->adData = $entry[0];
+		$this->ldapData = $entry[0];
 
-		return $this->adData;
+		return $this->ldapData;
 	}
 
 	public function mapGroups( User $user ) {
-		# Create a list of AD groups this person is a member of
+		# Create a list of LDAP groups this person is a member of
 		$memberOf = [];
-		if ( isset( $this->adData['memberof'] ) ) {
-			$tmp = array_map( 'strtolower',$this->adData['memberof'] );
+		if ( isset( $this->ldapData['memberof'] ) ) {
+			$tmp = array_map( 'strtolower',$this->ldapData['memberof'] );
 			unset( $tmp['count'] );
 			$memberOf = array_flip( $tmp );
 		}
 
-		# This is a list of AD groups that map to MW groups we already have
-		$hasControlledGroups = array_intersect( $this->adGroupMap,
+		# This is a list of LDAP groups that map to MW groups we already have
+		$hasControlledGroups = array_intersect( $this->ldapGroupMap,
 												$user->getGroups() );
 
 		# This is a list of groups that map to MW groups we do NOT already have
-		$notControlledGroups = array_diff( $this->adGroupMap,
+		$notControlledGroups = array_diff( $this->ldapGroupMap,
 										   $user->getGroups() );
 
-		# AD-mapped MW Groups that should be added because they aren't
+		# LDAP-mapped MW Groups that should be added because they aren't
 		# in the user's list of MW groups
 		$addThese = array_keys(
 			array_flip( array_intersect_key( $notControlledGroups,
 											 $memberOf ) ) );
 
 		# MW Groups that should be removed because the user doesn't have any
-		# of AD groups
+		# of LDAP groups
 		foreach ( array_keys( $this->mwGroupMap ) as $checkGroup ) {
 			$matched = array_intersect( $this->mwGroupMap[$checkGroup],
 										array_flip( $memberOf ) );
@@ -173,7 +173,7 @@ class LdapGroups {
 		global $IP;
 		$here = self::newFromIniFile( "$IP/ldap.ini" );
 
-		$here->fetchADData( $user, $email );
+		$here->fetchLDAPData( $user, $email );
 
 		// Make sure user is in the right groups;
 		$here->mapGroups( $user );
